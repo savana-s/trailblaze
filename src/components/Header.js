fbcebcb22836a3ddcb62e9872e8f4c9ff288ecfd
savana-s <savanasathre@gmail.com>
2020-03-25 01:57:48 -0700

    init

---
+++ src/components/Header.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+const Header = props => (
+  <header style={{ marginBottom: 10 }}>
+    <div>
+      <span className="header"> {props.title} </span>
+    </div>
+
+    <div className="subheader-body">
+      <span className="subheader">
+        {" "}
+        Source Code{" "}
+        <a
+          className="link"
+          target="_blank"
+          href="https://github.com/savana-s/trailblaze"
+        >
+          GitHub
+        </a>{" "}
+        |
+        <a
+          className="link"
+          target="_blank"
+          href="https://www.coindesk.com/price/"
+        >
+          Exchange Rate
+        </a>
+      </span>
+    </div>
+  </header>
+);
+
+export default Header;
